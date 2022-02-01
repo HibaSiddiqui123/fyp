3e41c3fa7553121a058a5d80b0a7ae410fd262eb
maftab <themaaz32@gmail.com>
2022-02-01 18:20:57 +0500

    fix: screen util issue

--- navbar_screens/lib/home1.dart
+++ navbar_screens/lib/home1.dart
@@ -9,50 +9,57 @@ class home1 extends StatefulWidget {
 }
 
 class _home1State extends State<home1> {
- 
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-      
-        child: Column(
-          
-          children: [
+        body: Center(
+      child: Column(
+        children: [
           Padding(
-              padding: const EdgeInsets.all(70.0),
-              child: Text("Hello User",style: TextStyle(fontSize:30,fontWeight: FontWeight.bold),
-    ),
-    
-    
-             ) ,
-        SliderButton(action: (){
+            padding: const EdgeInsets.all(70.0),
+            child: Text(
+              "Hello User",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SliderButton(
+              action: () {
 //functionality
-        },
-        label: Text("Swipe to Activate!",style: TextStyle(color:Colors.pinkAccent,fontSize: 15,fontWeight:FontWeight.w500, ),),
-        icon:Icon(Icons.power_settings_new
-        ,color: Colors.pinkAccent,
-        size: 40.0,
-        ),baseColor: Colors.black,
-        highlightedColor: Colors.yellow,
-        backgroundColor: Colors.pinkAccent
-        ),
-        
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Expanded(child: SvgPicture.asset("assets/pink-shield.svg",height:300,
-    
-  
+              },
+              label: Text(
+                "Swipe to Activate!",
+                style: TextStyle(
+                  color: Colors.pinkAccent,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              icon: Icon(
+                Icons.power_settings_new,
+                color: Colors.pinkAccent,
+                size: 40.0,
+              ),
+              baseColor: Colors.black,
+              highlightedColor: Colors.yellow,
+              backgroundColor: Colors.pinkAccent),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Expanded(
+              child: SvgPicture.asset(
+                "assets/pink-shield.svg",
+                height: 300,
+              ),
+            ),
           ),
-          
-          ),
-        ),
-          Text('"Happiness has many roots, but none more important than SECURITY..!"', style: TextStyle(fontSize:17,),textAlign: TextAlign.center,)
-             ],
-        ),
-        
-      )
-    
-      );
+          Text(
+            '"Happiness has many roots, but none more important than SECURITY..!"',
+            style: TextStyle(
+              fontSize: 17,
+            ),
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    ));
   }
 }
--- navbar_screens/lib/main.dart
+++ navbar_screens/lib/main.dart
@@ -1,13 +1,14 @@
+//@dart=2.9
+
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:navbar_screens/contact.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:navbar_screens/emergency.dart';
 import 'package:navbar_screens/home1.dart';
 import 'package:navbar_screens/phone.dart';
 import 'package:navbar_screens/setting.dart';
 
 import 'addcontact.dart';
-import 'splash1.dart';
 import 'splash2.dart';
 
 void main() {
@@ -15,85 +16,89 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner:false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      
-        primarySwatch: Colors.blue,
+    return  MediaQuery(
+      data: MediaQueryData.fromWindow(WidgetsBinding.instance.window),
+      child: ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: () => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: Splashh(),
+        ),
       ),
-    home: home1(),
     );
   }
 }
+
 // ignore: camel_case_types
 class homescreen extends StatefulWidget {
-
-
   @override
   _homescreenState createState() => _homescreenState();
 }
 
 class _homescreenState extends State<homescreen> {
- int index=0;
- final screens=[
-
-home1(),
-phone(),
-Addcontacts(),
-setting(),
-emegency(),
-
-];
-
-
+  int index = 0;
+  final screens = [
+    home1(),
+    phone(),
+    Addcontacts(),
+    setting(),
+    emegency(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final items =<Widget>[
-        Icon(Icons.home,size: 30,),
-          Icon(Icons.contact_phone,size: 30,),
-            Icon(Icons.people_alt_outlined,size: 30,),
-              Icon(Icons.settings,size: 30,),
-                Icon(Icons.notification_add_sharp,size: 30,),
-
-
+    final items = <Widget>[
+      Icon(
+        Icons.home,
+        size: 30,
+      ),
+      Icon(
+        Icons.contact_phone,
+        size: 30,
+      ),
+      Icon(
+        Icons.people_alt_outlined,
+        size: 30,
+      ),
+      Icon(
+        Icons.settings,
+        size: 30,
+      ),
+      Icon(
+        Icons.notification_add_sharp,
+        size: 30,
+      ),
     ];
-    
+
     return Scaffold(
-     drawer: setting(),
-      
-
-
-
-extendBody: true,
-body: screens[index],
-
-     bottomNavigationBar:Theme(
-data: Theme.of(context).copyWith(
-
-  iconTheme: IconThemeData(color: Colors.white)
-),
-
-       child: CurvedNavigationBar(
-         color: Colors.pinkAccent,
-         
-         backgroundColor: Colors.transparent,
-         height: 60,
-         animationCurve: Curves.easeInOut,
-         animationDuration: Duration(milliseconds: 250),
-         index: index,
-         items: items,
-         onTap: (index)=>setState(()=>this.index=index ),
-         
-         ),
-     ) ,
-
+      drawer: setting(),
+      extendBody: true,
+      body: screens[index],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context)
+            .copyWith(iconTheme: IconThemeData(color: Colors.white)),
+        child: CurvedNavigationBar(
+          color: Colors.pinkAccent,
+          backgroundColor: Colors.transparent,
+          height: 60,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 250),
+          index: index,
+          items: items,
+          onTap: (index) => setState(() => this.index = index),
+        ),
+      ),
     );
   }
 }
